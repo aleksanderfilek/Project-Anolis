14f32517a3772133afbfc086aad753ec4ec95d1c
Konrad Walas <kwalas314@gmail.com>
2020-07-31 20:55:43 +0200

    Add basic camera control

---
+++ Project Anolis/Assets/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform cameraControllerTransform;
+    [SerializeField] private Transform cameraTransform;
+
+    [SerializeField] private float rotatingSpeed = 100;
+    [SerializeField] private float zoomSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+            cameraControllerTransform.Rotate(new Vector3(1, 0, 0) * Time.deltaTime * rotatingSpeed);
+        if (Input.GetKey("s"))
+            cameraControllerTransform.Rotate(new Vector3(-1, 0, 0) * Time.deltaTime * rotatingSpeed);
+        if (Input.GetKey("a"))
+            cameraControllerTransform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * rotatingSpeed);
+        if (Input.GetKey("d"))
+            cameraControllerTransform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * rotatingSpeed);
+    }
+}
