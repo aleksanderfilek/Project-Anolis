647a759ef477be0b47930b84d5137da88634b212
Fillipek <mar.lim.420@onet.pl>
2020-08-04 19:19:04 +0200

    optimise code

--- Project Anolis/Assets/Scripts/PlanetGenerator.cs
+++ Project Anolis/Assets/Scripts/PlanetGenerator.cs
@@ -4,31 +4,12 @@ using UnityEngine.UIElements;
 
 public static class PlanetGenerator
 {
-    public struct Triangle
-    {
-        public int v1, v2, v3;
-
-        public Triangle(int v1, int v2, int v3)
-        {
-            this.v1 = v1;
-            this.v2 = v2;
-            this.v3 = v3;
-        }
-    }
-
-    static float _radius = 1f;
-    static int _iterations = 0;
-
     static List<Vector3> _verticesList;
-    static List<Triangle> _trianglesList;
+    static List<Vector3Int> _trianglesList; // each object keeps 3 vertex indices
 
     public static Mesh Create(float radius, int iterations)
     {
-        _radius = radius;
-        _iterations = iterations;
-
-        Initialize();
-        SeparateTriangles();
+        Initialize(radius);
         Subdivide();
 
         Mesh planetMesh = new Mesh();
@@ -38,9 +19,9 @@ public static class PlanetGenerator
         List<int> indices = new List<int>();
         foreach (var trinagle in _trianglesList)
         {
-            indices.Add(trinagle.v1);
-            indices.Add(trinagle.v2);
-            indices.Add(trinagle.v3);
+            indices.Add(trinagle.x);
+            indices.Add(trinagle.y);
+            indices.Add(trinagle.z);
         }
         planetMesh.triangles = indices.ToArray();
         planetMesh.RecalculateNormals();
@@ -48,85 +29,68 @@ public static class PlanetGenerator
         return planetMesh;
     }
 
-    static void Initialize()
+    // creates a set of sepearate triangles creating icosahedron
+    static void Initialize(float radius)
     {
         _verticesList = new List<Vector3>();
-        _trianglesList = new List<Triangle>();
+        _trianglesList = new List<Vector3Int>();
 
         float t = (1f + Mathf.Sqrt(5f)) / 2f;
+        List<Vector3> tVectors = new List<Vector3>();
 
-        _verticesList.Add(new Vector3(-1, t, 0).normalized * _radius);
-        _verticesList.Add(new Vector3(1, t, 0).normalized * _radius);
-        _verticesList.Add(new Vector3(-1, -t, 0).normalized * _radius);
-        _verticesList.Add(new Vector3(1, -t, 0).normalized * _radius);
+        tVectors.Add(new Vector3(-1, t, 0).normalized * radius);
+        tVectors.Add(new Vector3(1, t, 0).normalized * radius);
+        tVectors.Add(new Vector3(-1, -t, 0).normalized * radius);
+        tVectors.Add(new Vector3(1, -t, 0).normalized * radius);
 
-        _verticesList.Add(new Vector3(0, -1, t).normalized * _radius);
-        _verticesList.Add(new Vector3(0, 1, t).normalized * _radius);
-        _verticesList.Add(new Vector3(0, -1, -t).normalized * _radius);
-        _verticesList.Add(new Vector3(0, 1, -t).normalized * _radius);
+        tVectors.Add(new Vector3(0, -1, t).normalized * radius);
+        tVectors.Add(new Vector3(0, 1, t).normalized * radius);
+        tVectors.Add(new Vector3(0, -1, -t).normalized * radius);
+        tVectors.Add(new Vector3(0, 1, -t).normalized * radius);
 
-        _verticesList.Add(new Vector3(t, 0, -1).normalized * _radius);
-        _verticesList.Add(new Vector3(t, 0, 1).normalized * _radius);
-        _verticesList.Add(new Vector3(-t, 0, -1).normalized * _radius);
-        _verticesList.Add(new Vector3(-t, 0, 1).normalized * _radius);
+        tVectors.Add(new Vector3(t, 0, -1).normalized * radius);
+        tVectors.Add(new Vector3(t, 0, 1).normalized * radius);
+        tVectors.Add(new Vector3(-t, 0, -1).normalized * radius);
+        tVectors.Add(new Vector3(-t, 0, 1).normalized * radius);
 
-        _trianglesList.Add(new Triangle(0, 11, 5));
-        _trianglesList.Add(new Triangle(0, 5, 1));
-        _trianglesList.Add(new Triangle(0, 1, 7));
-        _trianglesList.Add(new Triangle(0, 7, 10));
-        _trianglesList.Add(new Triangle(0, 10, 11));
+        AddTriangle(tVectors[0], tVectors[11], tVectors[5]);
+        AddTriangle(tVectors[0], tVectors[5], tVectors[1]);
+        AddTriangle(tVectors[0], tVectors[1], tVectors[7]);
+        AddTriangle(tVectors[0], tVectors[7], tVectors[10]);
+        AddTriangle(tVectors[0], tVectors[10], tVectors[11]);
 
-        _trianglesList.Add(new Triangle(1, 5, 9));
-        _trianglesList.Add(new Triangle(5, 11, 4));
-        _trianglesList.Add(new Triangle(11, 10, 2));
-        _trianglesList.Add(new Triangle(10, 7, 6));
-        _trianglesList.Add(new Triangle(7, 1, 8));
+        AddTriangle(tVectors[1], tVectors[5], tVectors[9]);
+        AddTriangle(tVectors[5], tVectors[11], tVectors[4]);
+        AddTriangle(tVectors[11], tVectors[10], tVectors[2]);
+        AddTriangle(tVectors[10], tVectors[7], tVectors[6]);
+        AddTriangle(tVectors[7], tVectors[1], tVectors[8]);
 
-        _trianglesList.Add(new Triangle(3, 9, 4));
-        _trianglesList.Add(new Triangle(3, 4, 2));
-        _trianglesList.Add(new Triangle(3, 2, 6));
-        _trianglesList.Add(new Triangle(3, 6, 8));
-        _trianglesList.Add(new Triangle(3, 8, 9));
+        AddTriangle(tVectors[3], tVectors[9], tVectors[4]);
+        AddTriangle(tVectors[3], tVectors[4], tVectors[2]);
+        AddTriangle(tVectors[3], tVectors[2], tVectors[6]);
+        AddTriangle(tVectors[3], tVectors[6], tVectors[8]);
+        AddTriangle(tVectors[3], tVectors[8], tVectors[9]);
 
-        _trianglesList.Add(new Triangle(4, 9, 5));
-        _trianglesList.Add(new Triangle(2, 4, 11));
-        _trianglesList.Add(new Triangle(6, 2, 10));
-        _trianglesList.Add(new Triangle(8, 6, 7));
-        _trianglesList.Add(new Triangle(9, 8, 1));
+        AddTriangle(tVectors[4], tVectors[9], tVectors[5]);
+        AddTriangle(tVectors[2], tVectors[4], tVectors[11]);
+        AddTriangle(tVectors[6], tVectors[2], tVectors[10]);
+        AddTriangle(tVectors[8], tVectors[6], tVectors[7]);
+        AddTriangle(tVectors[9], tVectors[8], tVectors[1]);
     }
 
-    static void SeparateTriangles()
-    {
-        // first clone vertices to have unique one for each triangle
-        // each vertex makes 6 triangles
-        int numOfVertices = _verticesList.Count;
-        for (int i = 0; i < 5; i++)
-            for(int j=0; j<numOfVertices; j++)
-                _verticesList.Add(_verticesList[j]);
-
-        // now triangles
-        // to make sure we used unique vertex we need additional array
-        // it keep info about number of usages for each vertex
-        int[] usagesCounter = new int[_verticesList.Count / 6];
-        for(int i=0; i<_trianglesList.Count; i++)
-        {
-            int indexV1 = _trianglesList[i].v1;
-            int indexV2 = _trianglesList[i].v2;
-            int indexV3 = _trianglesList[i].v3;
-
-            usagesCounter[indexV1]++;
-            indexV1 += usagesCounter[indexV1] * usagesCounter.Length;
-            usagesCounter[indexV2]++;
-            indexV2 += usagesCounter[indexV2] * usagesCounter.Length;
-            usagesCounter[indexV3]++;
-            indexV3 += usagesCounter[indexV3] * usagesCounter.Length;
-
-            _trianglesList[i] = new Triangle(indexV1, indexV2, indexV3);
-        }
-    }
-
+    // iteratively subdivides each triangle into 4 triangles
     static void Subdivide()
     {
 
+    }
+
+    // adds a triangle into the end of lists
+    static void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        int idx = _verticesList.Count;
+        _verticesList.Add(v1.normalized);
+        _verticesList.Add(v2.normalized);
+        _verticesList.Add(v3.normalized);
+        _trianglesList.Add(new Vector3Int(idx, idx + 1, idx + 2));
     }
 }
