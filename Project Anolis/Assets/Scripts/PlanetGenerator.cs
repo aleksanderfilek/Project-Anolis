5300f0b60c6ca3359edf0489d8890b74fbf7e56f
Fillipek <mar.lim.420@onet.pl>
2020-08-05 14:19:55 +0200

    add iterative sibdivision and custom randomization

--- Project Anolis/Assets/Scripts/PlanetGenerator.cs
+++ Project Anolis/Assets/Scripts/PlanetGenerator.cs
@@ -1,96 +1,154 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public static class PlanetGenerator
 {
-    static List<Vector3> _verticesList;
-    static List<Vector3Int> _trianglesList; // each object keeps 3 vertex indices
-
-    public static Mesh Create(float radius, int iterations)
+    // creates a planet of given radius
+    // iteratively subdivides a suface desired times
+    // and randomises heights of vertices seed and with given intensivity
+    // skip intensivity if you do not want randomizing
+    // skip seed if you want it random
+    public static Mesh Generate(float radius, int iterations = 0, float intensivity = 0f, int seed = -1)
     {
-        Initialize(radius);
-        Subdivide();
-
+        List<Vector3> vertexList = new List<Vector3>();
+        List<Vector3Int> triangleList = new List<Vector3Int>(); 
         Mesh planetMesh = new Mesh();
 
-        planetMesh.Clear();
-        planetMesh.vertices = _verticesList.ToArray();
-        List<int> indices = new List<int>();
-        foreach (var trinagle in _trianglesList)
-        {
-            indices.Add(trinagle.x);
-            indices.Add(trinagle.y);
-            indices.Add(trinagle.z);
-        }
-        planetMesh.triangles = indices.ToArray();
-        planetMesh.RecalculateNormals();
+        Initialize(vertexList, triangleList, radius);
+        if(iterations > 0)
+            Subdivide(iterations, vertexList, ref triangleList, radius);
+        if(intensivity > 0f)
+            Randomize(vertexList, radius, intensivity, seed);
+        BuildSphere(planetMesh, vertexList, triangleList);
 
         return planetMesh;
     }
 
-    // creates a set of sepearate triangles creating icosahedron
-    static void Initialize(float radius)
+    // creates an icosahedron of given radius (distance from (0,0,0) to vertex)
+    private static void Initialize(List<Vector3> vertexList, List<Vector3Int> triangleList, float radius)
     {
-        _verticesList = new List<Vector3>();
-        _trianglesList = new List<Vector3Int>();
-
         float t = (1f + Mathf.Sqrt(5f)) / 2f;
-        List<Vector3> tVectors = new List<Vector3>();
 
-        tVectors.Add(new Vector3(-1, t, 0).normalized * radius);
-        tVectors.Add(new Vector3(1, t, 0).normalized * radius);
-        tVectors.Add(new Vector3(-1, -t, 0).normalized * radius);
-        tVectors.Add(new Vector3(1, -t, 0).normalized * radius);
+        vertexList.Add(new Vector3(-1, t, 0).normalized * radius);
+        vertexList.Add(new Vector3(1, t, 0).normalized * radius);
+        vertexList.Add(new Vector3(-1, -t, 0).normalized * radius);
+        vertexList.Add(new Vector3(1, -t, 0).normalized * radius);
 
-        tVectors.Add(new Vector3(0, -1, t).normalized * radius);
-        tVectors.Add(new Vector3(0, 1, t).normalized * radius);
-        tVectors.Add(new Vector3(0, -1, -t).normalized * radius);
-        tVectors.Add(new Vector3(0, 1, -t).normalized * radius);
+        vertexList.Add(new Vector3(0, -1, t).normalized * radius);
+        vertexList.Add(new Vector3(0, 1, t).normalized * radius);
+        vertexList.Add(new Vector3(0, -1, -t).normalized * radius);
+        vertexList.Add(new Vector3(0, 1, -t).normalized * radius);
 
-        tVectors.Add(new Vector3(t, 0, -1).normalized * radius);
-        tVectors.Add(new Vector3(t, 0, 1).normalized * radius);
-        tVectors.Add(new Vector3(-t, 0, -1).normalized * radius);
-        tVectors.Add(new Vector3(-t, 0, 1).normalized * radius);
+        vertexList.Add(new Vector3(t, 0, -1).normalized * radius);
+        vertexList.Add(new Vector3(t, 0, 1).normalized * radius);
+        vertexList.Add(new Vector3(-t, 0, -1).normalized * radius);
+        vertexList.Add(new Vector3(-t, 0, 1).normalized * radius);
 
-        AddTriangle(tVectors[0], tVectors[11], tVectors[5]);
-        AddTriangle(tVectors[0], tVectors[5], tVectors[1]);
-        AddTriangle(tVectors[0], tVectors[1], tVectors[7]);
-        AddTriangle(tVectors[0], tVectors[7], tVectors[10]);
-        AddTriangle(tVectors[0], tVectors[10], tVectors[11]);
+        triangleList.Add(new Vector3Int(0, 11, 5));
+        triangleList.Add(new Vector3Int(0, 5, 1));
+        triangleList.Add(new Vector3Int(0, 1, 7));
+        triangleList.Add(new Vector3Int(0, 7, 10));
+        triangleList.Add(new Vector3Int(0, 10, 11));
 
-        AddTriangle(tVectors[1], tVectors[5], tVectors[9]);
-        AddTriangle(tVectors[5], tVectors[11], tVectors[4]);
-        AddTriangle(tVectors[11], tVectors[10], tVectors[2]);
-        AddTriangle(tVectors[10], tVectors[7], tVectors[6]);
-        AddTriangle(tVectors[7], tVectors[1], tVectors[8]);
+        triangleList.Add(new Vector3Int(1, 5, 9));
+        triangleList.Add(new Vector3Int(5, 11, 4));
+        triangleList.Add(new Vector3Int(11, 10, 2));
+        triangleList.Add(new Vector3Int(10, 7, 6));
+        triangleList.Add(new Vector3Int(7, 1, 8));
 
-        AddTriangle(tVectors[3], tVectors[9], tVectors[4]);
-        AddTriangle(tVectors[3], tVectors[4], tVectors[2]);
-        AddTriangle(tVectors[3], tVectors[2], tVectors[6]);
-        AddTriangle(tVectors[3], tVectors[6], tVectors[8]);
-        AddTriangle(tVectors[3], tVectors[8], tVectors[9]);
+        triangleList.Add(new Vector3Int(3, 9, 4));
+        triangleList.Add(new Vector3Int(3, 4, 2));
+        triangleList.Add(new Vector3Int(3, 2, 6));
+        triangleList.Add(new Vector3Int(3, 6, 8));
+        triangleList.Add(new Vector3Int(3, 8, 9));
 
-        AddTriangle(tVectors[4], tVectors[9], tVectors[5]);
-        AddTriangle(tVectors[2], tVectors[4], tVectors[11]);
-        AddTriangle(tVectors[6], tVectors[2], tVectors[10]);
-        AddTriangle(tVectors[8], tVectors[6], tVectors[7]);
-        AddTriangle(tVectors[9], tVectors[8], tVectors[1]);
+        triangleList.Add(new Vector3Int(4, 9, 5));
+        triangleList.Add(new Vector3Int(2, 4, 11));
+        triangleList.Add(new Vector3Int(6, 2, 10));
+        triangleList.Add(new Vector3Int(8, 6, 7));
+        triangleList.Add(new Vector3Int(9, 8, 1));
     }
 
-    // iteratively subdivides each triangle into 4 triangles
-    static void Subdivide()
+    // divides each triangle iteratively into 4 triangles 
+    // and moves new vertices to be in desired radius from zero point
+    private static void Subdivide(int numOfIterations, List<Vector3> vertexList, ref List<Vector3Int> triangleList, float radius)
     {
+        Dictionary<long, int> cache = new Dictionary<long, int>();
+        for(int iteration=0; iteration < numOfIterations; iteration++)
+        {
+            var newTriangleList = new List<Vector3Int>();
+            foreach(var triangle in triangleList)
+            {
+                int a = getMidPoint(triangle.x, triangle.y, ref vertexList, ref cache, radius);
+                int b = getMidPoint(triangle.y, triangle.z, ref vertexList, ref cache, radius);
+                int c = getMidPoint(triangle.z, triangle.x, ref vertexList, ref cache, radius);
 
+                newTriangleList.Add(new Vector3Int(triangle.x, a, c));
+                newTriangleList.Add(new Vector3Int(triangle.y, b, a));
+                newTriangleList.Add(new Vector3Int(triangle.z, c, b));
+                newTriangleList.Add(new Vector3Int(a, b, c));
+            }
+            triangleList = newTriangleList;
+        }
     }
 
-    // adds a triangle into the end of lists
-    static void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+    // applies noise to each vertex distance from the middle of sphere
+    private static void Randomize(List<Vector3> vertexList, float radius, float intensivity, int seed)
     {
-        int idx = _verticesList.Count;
-        _verticesList.Add(v1.normalized);
-        _verticesList.Add(v2.normalized);
-        _verticesList.Add(v3.normalized);
-        _trianglesList.Add(new Vector3Int(idx, idx + 1, idx + 2));
+        if (seed != -1) Random.InitState(seed);
+
+        for(int i=0; i<vertexList.Count; i++)
+        {
+            vertexList[i] *= Random.Range(1 - intensivity, 1 + intensivity);
+        }
+    }
+
+    // builds arrays of vertices and triangles using references to achieve flat shading
+    // and applies new arrays to target mesh
+    private static void BuildSphere(Mesh targetMesh, List<Vector3> vertexList, List<Vector3Int> triangleList)
+    {
+        List<Vector3> newVertexList = new List<Vector3>();
+        int[] indices = new int[triangleList.Count * 3];
+
+        for(int i=0; i<indices.Length; i+=3)
+        {
+            indices[i + 0] = i + 0;
+            indices[i + 1] = i + 1;
+            indices[i + 2] = i + 2;
+
+            newVertexList.Add(vertexList[triangleList[i/3].x]);
+            newVertexList.Add(vertexList[triangleList[i/3].y]);
+            newVertexList.Add(vertexList[triangleList[i/3].z]);
+        }
+
+        targetMesh.Clear();
+        targetMesh.vertices = newVertexList.ToArray();
+        targetMesh.triangles = indices;
+        targetMesh.RecalculateNormals();
+        targetMesh.Optimize();
+    }
+
+    private static int getMidPoint(int p1, int p2, ref List<Vector3> vertexList, ref Dictionary<long, int> cache, float radius)
+    {
+        bool isFirstSmaller = p1 < p2;
+        long smallerIndex = isFirstSmaller ? p1 : p2;
+        long greaterIndex = isFirstSmaller ? p2 : p1;
+        long key = (smallerIndex << 32) + greaterIndex;
+
+        int ret;
+        if(cache.TryGetValue(key, out ret))
+        {
+            return ret;
+        }
+
+        Vector3 point1 = vertexList[p1];
+        Vector3 point2 = vertexList[p2];
+        Vector3 middle = new Vector3((point1.x + point2.x) / 2f, (point1.y + point2.y) / 2f, (point1.z + point2.z) / 2f);
+
+        int i = vertexList.Count;
+        vertexList.Add(middle.normalized * radius);
+        cache.Add(key, i);
+
+        return i;
     }
 }
