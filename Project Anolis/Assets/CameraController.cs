88396784054f9086e184f74f7d3f95ddb7a058f9
Konrad Walas <kwalas314@gmail.com>
2020-08-09 17:53:11 +0200

    DRY the code

--- Project Anolis/Assets/CameraController.cs
+++ Project Anolis/Assets/CameraController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.SocialPlatforms;
 
 public class CameraController : MonoBehaviour
@@ -19,48 +21,49 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
+        UpdateInput();
+        MakeMovement();
+    }
+
+    private void UpdateInput()
+    {
         _rotationVertical = Input.GetAxis("Vertical");
         _rotationHorizontal = Input.GetAxis("Horizontal");
         _zoom = Input.GetAxis("Mouse ScrollWheel");
-
-        MakeMovement();
     }
 
     private void MakeMovement()
     {
-        if (_rotationVertical > 0 && isWithinUpperBound())
-            rotateCamera(CameraRotateDirection.up, amount: Mathf.Abs(_rotationVertical));
+        if (_rotationVertical != 0 && IsWithinBounds())
+            rotateCamera(direction: new Vector3(-1, 0, 0), amount: _rotationVertical);
 
-        if (_rotationVertical < 0 && isWithinLowerBound())
-            rotateCamera(CameraRotateDirection.down, amount: Mathf.Abs(_rotationVertical));
+        if (_rotationHorizontal != 0)
+            rotateCamera(direction: new Vector3(0, -1, 0), amount: _rotationHorizontal, relativeTo: Space.World);
 
-        if (_rotationHorizontal < 0)
-            rotateCamera(CameraRotateDirection.left, amount: Mathf.Abs(_rotationHorizontal), relativeTo: Space.World);
-
-        if (_rotationHorizontal > 0)
-            rotateCamera(CameraRotateDirection.right, amount: Mathf.Abs(_rotationHorizontal), relativeTo: Space.World);
-
-        if (_zoom > 0f)
-            ZoomIn();
-
-        if (_zoom < 0f)
-            ZoomOut();
+        if (_zoom != 0f)
+            Zoom();
     }
 
-    private bool isWithinUpperBound()
+    private bool IsWithinBounds()
     {
+        var rotationAngles = cameraControllerTransform.localEulerAngles;
+        
         //sould be rotationEulerAngles.z == 180f, but comparing floats is not a good idea
         //rotationEulerAngles.z can have values 180f and 0f, therefore conidtion > 90f was chosen
-        var rotationAngles = cameraControllerTransform.localEulerAngles;
-        return rotationAngles.z > 90f ? rotationAngles.x < 90f : true;
+        if (rotationAngles.z < 90f)
+            return true;
+
+        return _rotationVertical > 0f ? IsWithinUpperBoundary() : IsWithinLowerBoundary();
     }
 
-    private bool isWithinLowerBound()
+    private bool IsWithinUpperBoundary()
     {
-        //sould be rotationEulerAngles.z == 180f, but comparing floats is not a good idea
-        //rotationEulerAngles.z can have values 180f and 0f, therefore conidtion > 90f was chosen
-        var rotationAngles = cameraControllerTransform.localEulerAngles;
-        return rotationAngles.z > 90f ? rotationAngles.x > 270f : true;
+        return cameraControllerTransform.localEulerAngles.x < 90f;
+    }
+
+    private bool IsWithinLowerBoundary()
+    {
+        return cameraControllerTransform.localEulerAngles.x > 270f;
     }
 
     private void rotateCamera(Vector3 direction, float amount, Space relativeTo = Space.Self)
@@ -68,34 +71,17 @@ public class CameraController : MonoBehaviour
         cameraControllerTransform.Rotate(direction * Time.deltaTime * rotatingSpeed * amount, relativeTo);
     }
 
-    static class CameraRotateDirection
-    {
-        public static readonly Vector3 down = new Vector3(1, 0, 0);
-        public static readonly Vector3 up = new Vector3(-1, 0, 0);
-        public static readonly Vector3 right = new Vector3(0, -1, 0);
-        public static readonly Vector3 left = new Vector3(0, 1, 0);
-    }
-
-    private void ZoomIn()
-    {
-        Zoom(direction: new Vector3(0, 0, -1));
-    }
-
-    private void ZoomOut()
-    {
-        Zoom(direction: new Vector3(0, 0, 1));
-    }
-
-    private void Zoom(Vector3 direction)
+    private void Zoom()
     {
         cameraTransform.localPosition += new Vector3(0, 0, -1) * zoomSpeed * Time.deltaTime * _zoom;
+        if (cameraTransform.localPosition.z < planetSurfaceHeight)
+            FixCameraHeight();
+    }
 
+    private void FixCameraHeight()
+    {
         var cameraPosition = cameraTransform.localPosition;
-        if (cameraPosition.z < planetSurfaceHeight)
-        {
-            cameraPosition.z = planetSurfaceHeight;
-            cameraTransform.localPosition = cameraPosition;
-        }
-            
+        cameraPosition.z = planetSurfaceHeight;
+        cameraTransform.localPosition = cameraPosition;
     }
 }
