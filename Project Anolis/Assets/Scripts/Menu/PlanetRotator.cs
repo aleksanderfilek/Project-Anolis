571b2eadc81db52a094123f0ffd8aafdae15096c
Aleksander Filek <filekaleksander@gmail.com>
2020-08-10 19:44:49 +0200

    generic staff

---
+++ Project Anolis/Assets/Scripts/Menu/PlanetRotator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Menu
+{
+    public class PlanetRotator : MonoBehaviour
+    {
+        [SerializeField] private Vector3 vector;
+
+        private void Update()
+        {
+            transform.Rotate(vector);
+        }
+    }
+}
--- Project Anolis/Assets/Scripts/Menu/SettingsMenuManager.cs
+++ Project Anolis/Assets/Scripts/Menu/SettingsMenuManager.cs
@@ -1,13 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 namespace Menu
 {
     public class SettingsMenuManager : MonoBehaviour
     {
+        [SerializeField] private GameObject[] groups;
+        private int _currentGroupIndex = 0;
+
+        private void Start()
+        {
+            groups[0].SetActive(true);
+            for (int i = 1; i < groups.Length; i++)
+            {
+                groups[i].SetActive(false);
+            }
+        }
+
+        public void ChangeGroup_Click(int index)
+        {
+            groups[_currentGroupIndex].SetActive(false);
+            _currentGroupIndex = index;
+            groups[_currentGroupIndex].SetActive(false);
+        }
+        
         public void Back_Click()
         {
+            ChangeGroup_Click(0);
             MainMenuManager.Get.ChangeCanvasElement_Click(0);
         }
 
