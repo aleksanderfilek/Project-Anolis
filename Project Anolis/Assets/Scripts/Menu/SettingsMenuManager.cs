d79f0a47a7d6a775ff6e7310350005b6bd306f9d
Aleksander Filek <filekaleksander@gmail.com>
2020-08-21 18:17:59 +0200

    graphics settings

--- Project Anolis/Assets/Scripts/Menu/SettingsMenuManager.cs
+++ Project Anolis/Assets/Scripts/Menu/SettingsMenuManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Menu
 {
@@ -9,14 +10,43 @@ namespace Menu
     {
         [SerializeField] private GameObject[] groups;
         private int _currentGroupIndex = 0;
+        
+        [Header("Graphics")]
+        [SerializeField] private Dropdown resolutionDropdown;
+        private Resolution[] _resolutions;
 
         private void Start()
         {
+            #region GROUPS
             groups[0].SetActive(true);
-            for (int i = 1; i < groups.Length; i++)
+            for (var i = 1; i < groups.Length; i++)
             {
                 groups[i].SetActive(false);
             }
+            #endregion
+            
+            #region RESOLUTION
+            _resolutions = Screen.resolutions;
+
+            resolutionDropdown.ClearOptions();
+            var options = new List<string>();
+
+            var currentResolutionIndex = 0;
+
+            for(var i = _resolutions.Length - 1; i >= 0; i--)
+            {
+                string option = _resolutions[i].width+"x"+_resolutions[i].height;
+                options.Add(option);
+
+                if(_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height){
+                    currentResolutionIndex = i;
+                }
+            }
+
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
+            #endregion
         }
 
         public void ChangeGroup_Click(int index)
@@ -36,10 +66,19 @@ namespace Menu
         {
             
         }
-
+        
         public void Save_Click()
         {
             
         }
+        
+        public void SetFullscreen(bool flag){
+            Screen.fullScreen = flag;
+        }
+
+        public void SetResolution(int resolutionIndex){
+            Resolution resolution = _resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        }  
     }
 }
