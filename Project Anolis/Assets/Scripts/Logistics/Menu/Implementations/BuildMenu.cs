81fadb2fc996524ec075fa8882f15145bbbc06c4
Marcin Filipek <56352792+Fillipek@users.noreply.github.com>
2020-09-18 19:53:41 +0200

    Make BuildMenu show on the mouse position

--- Project Anolis/Assets/Scripts/Logistics/Menu/Implementations/BuildMenu.cs
+++ Project Anolis/Assets/Scripts/Logistics/Menu/Implementations/BuildMenu.cs
@@ -25,6 +25,34 @@ namespace Logistics
                 return false;
             return _tileSelector.SelectedTile.TileContent == null;
         }
+        
+        public override void Show()
+        {
+            _uiCanvas.enabled = true;
+
+            var halfMenuSize = _uiRadial.rect.size / 2;
+            var screenSize = _uiCanvas.pixelRect.size;
+            
+            var newPosition = Input.mousePosition;
+
+            newPosition.x = newPosition.x <= screenSize.x - halfMenuSize.x
+                ? newPosition.x
+                : screenSize.x - halfMenuSize.x;
+
+            newPosition.x = newPosition.x >= halfMenuSize.x
+                ? newPosition.x
+                : halfMenuSize.x;
+            
+            newPosition.y = newPosition.y <= screenSize.y - halfMenuSize.y
+                ? newPosition.y
+                : screenSize.y - halfMenuSize.y;
+
+            newPosition.y = newPosition.y >= halfMenuSize.y
+                ? newPosition.y
+                : halfMenuSize.y;
+
+            _uiRadial.position = newPosition;
+        }
 
         public void BuildForge()
         {
