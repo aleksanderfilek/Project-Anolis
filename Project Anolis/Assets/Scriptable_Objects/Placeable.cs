f3def36bcaae89ab4475602c7dab6bbe4368b1fe
Marcin Filipek <marcin.filipek.321@gmail.com>
2020-10-19 16:57:13 +0200

    Add descriptions to Placeables

--- Project Anolis/Assets/Scriptable_Objects/Placeable.cs
+++ Project Anolis/Assets/Scriptable_Objects/Placeable.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Tile Object", menuName = "Scriptable Objects/Tile Object")]
+[CreateAssetMenu(fileName = "New Placeable", menuName = "Scriptable Objects")]
 public class Placeable : ScriptableObject
 {
     public string objectName;
     public ObjectType objectType;
     public GameObject prefab;
+    [TextAreaAttribute(15,20)] public string description;
 }
