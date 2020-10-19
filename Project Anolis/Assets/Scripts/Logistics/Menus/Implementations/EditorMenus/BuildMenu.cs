11012450005e8fe422c64512c678a8517f22b2cf
Marcin Filipek <marcin.filipek.321@gmail.com>
2020-10-19 16:40:06 +0200

    Add button generation for editor menus

--- Project Anolis/Assets/Scripts/Logistics/Menus/Implementations/EditorMenus/BuildMenu.cs
+++ Project Anolis/Assets/Scripts/Logistics/Menus/Implementations/EditorMenus/BuildMenu.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Logistics.Editor
 {
     public class BuildMenu : Menu
     {
+        [SerializeField] private List<Placeable> _placeables;
+        [SerializeField] private GameObject _buttonPrefab;
+
         public override bool CanHandleSelection()
         {
             return false;
+        }
+
+        public void Start()
+        {
+            if (_placeables.Count == 0)
+            {
+                Debug.Log(this + ": List of placeables is empty");
+                return;
+            }
+
+            foreach (var placeable in _placeables)
+            {
+                var newButton = Instantiate(_buttonPrefab, Ui.transform);
+                newButton.GetComponentInChildren<Text>().text = placeable.objectName;
+            }
         }
     }
 }
