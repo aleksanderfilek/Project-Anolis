b37667ab50585ee6438de3eea66a4f7d1a4bee57
Konrad Walas <kwalas314@gmail.com>
2020-10-19 21:34:58 +0200

    Separated Menu Group into two classes - one with default menu that is activated whenever another menu desactivates, and other without default menu

---
+++ Project Anolis/Assets/Scripts/Logistics/MenuGroups/MenuGroup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Logistics
+{
+    public class MenuGroup : MonoBehaviour
+    {
+        [SerializeField] private Showable _openOnAwakeMenu;
+        private Showable _currentMenu;
+
+        private void Awake()
+        {
+            ActivateMenu(_openOnAwakeMenu);
+        }
+
+        public void ActivateMenu(Showable menu)
+        {
+            _currentMenu = menu;
+            _currentMenu.Show();
+        }
+
+        public void DeactivateCurrent()
+        {
+            _currentMenu.Hide();
+        }
+    }
+}
---
+++ Project Anolis/Assets/Scripts/Logistics/MenuGroups/MenuGroupWithDefault.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Logistics
+{
+    public class MenuGroupWithDefault : MonoBehaviour
+    {
+        [SerializeField] private Showable _defaultMenu;
+        private Showable _currentMenu;
+
+        public void Awake()
+        {
+            ActivateMenu(_defaultMenu);
+        }
+
+        public void ActivateMenu(Showable menu)
+        {
+            _currentMenu = menu;
+            _currentMenu.Show();
+        }
+
+        public void ChangeCurrentToDefault()
+        {
+            _currentMenu.Hide();
+            ActivateMenu(_defaultMenu);
+        }
+    }
+}
