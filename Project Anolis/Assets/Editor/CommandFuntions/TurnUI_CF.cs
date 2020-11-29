b0455282885416527f0ba40a2d00c7f738c2f8fe
Aleksander Filek <filekaleksander@gmail.com>
2020-11-29 21:15:51 +0100

    TurnUI - command function

---
+++ Project Anolis/Assets/Editor/CommandFuntions/TurnUI_CF.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Editor.CommandFuntions
+{
+    public class TurnUI_CF
+    {
+        public static bool Process(string[] args)
+        {
+            if (Application.isPlaying == false)
+                return false;
+
+            var canvasGroups = Object.FindObjectsOfType<CanvasGroup>();
+            foreach (var cg in canvasGroups)
+            {
+                cg.alpha = ((int)cg.alpha == 1) ? 0 : 1;
+                cg.interactable = !cg.interactable;
+            }
+            
+            return true;
+        }
+    }
+}
