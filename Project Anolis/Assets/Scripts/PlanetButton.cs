4df5327b9848990556ab3fb23ba8e5e33e62e03e
Fillipek <mar.lim.420@onet.pl>
2020-12-01 19:08:19 +0100

    Add quick switching camera focus

--- Project Anolis/Assets/Scripts/PlanetButton.cs
+++ Project Anolis/Assets/Scripts/PlanetButton.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 
 // This is a component to allow button keeping a reference
-// to a planet assigned by PlanetButtonAssigner
+// to a planet assigned by PlaneMenu
 public class PlanetButton : MonoBehaviour
 {
     public GameObject Planet { get; set; }
+
+    public void ChangeFocusToThis()
+    {
+        GameState.Get.CurrentFocus = Planet.gameObject;
+        GameState.Get.ChangeModeToPlanetary();
+    }
 }
--- Project Anolis/Assets/Scripts/PlanetMenu.cs
+++ Project Anolis/Assets/Scripts/PlanetMenu.cs
@@ -2,67 +2,76 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-// This scripts a new button for each GameObject with "Planet" Tag.
-// Assigns buttons as a children of given panel
-public class PlanetMenu : MonoBehaviour
+namespace Interaction.Editor
 {
-    [SerializeField] private GameObject panel;
-    [SerializeField] private GameObject buttonPrefab;
-    private List<GameObject> planets;
-
-    private void Start()
+    // This scripts a new button for each GameObject with "Planet" Tag.
+    // Assigns buttons as a children of given panel
+    public class PlanetMenu : Menu
     {
-        CheckSerializedFields();
-        GetAllPlanets();
-        foreach (var planet in planets)
+        [SerializeField] private GameObject buttonPrefab;
+        private List<GameObject> planets;
+    
+        private void Start()
+        {
+            CheckSerializedFields();
+            GetAllPlanets();
+            foreach (var planet in planets)
+            {
+                AssignPlanetToButton(planet, CreateButton());
+            }
+        }
+    
+        // Adds a new button and assigns to it passed planet.
+        // Does not check if this planet is already in list.
+        public void AddPlanet(GameObject planet)
         {
             AssignPlanetToButton(planet, CreateButton());
         }
-    }
-
-    // Adds a new button and assigns to it passed planet.
-    // Does not check if this planet is already in list.
-    public void AddPlanet(GameObject planet)
-    {
-        AssignPlanetToButton(planet, CreateButton());
-    }
-
-    private void GetAllPlanets()
-    {
-        var planetCandidates = GameObject.FindGameObjectsWithTag("Planet");
-        var planetList = new List<GameObject>();
-        foreach (var planet in planetCandidates)
+        
+        public override bool CanHandleSelection()
         {
-            if (planet.GetComponent<Planet>() is null)
-            {
-                Debug.LogWarning(this + "One of object with tag: \"Planet\" does not have \"Planet\" component attached.");
-                continue;
-            }
-            planetList.Add(planet);
+            return false;
         }
-
-        planets = planetList;
-    }
-
-    private GameObject CreateButton()
-    {
-        var button = Instantiate(buttonPrefab, panel.transform); 
-        button.name = "Button Planet";
-        return button;
-    }
-
-    private void AssignPlanetToButton(GameObject planet, GameObject button)
-    {
-        button.AddComponent<PlanetButton>().Planet = planet;
-        button.GetComponentInChildren<Text>().text = planet.name;
-    }
-
-    private void CheckSerializedFields()
-    {
-        if (panel is null) 
-            Debug.LogError(": Panel not assigned",this);
-
-        if (buttonPrefab is null)
-            Debug.LogError(": Button Prefab not assigned.", this);
+    
+        private void GetAllPlanets()
+        {
+            var planetCandidates = GameObject.FindGameObjectsWithTag("Planet");
+            var planetList = new List<GameObject>();
+            foreach (var planet in planetCandidates)
+            {
+                if (planet.GetComponent<Planet>() is null)
+                {
+                    Debug.LogWarning(this + "One of object with tag: \"Planet\" does not have \"Planet\" component attached.");
+                    continue;
+                }
+                planetList.Add(planet);
+            }
+    
+            planets = planetList;
+        }
+    
+        private GameObject CreateButton()
+        {
+            var button = Instantiate(buttonPrefab, ui.transform); 
+            button.name = "Button Planet";
+            return button;
+        }
+    
+        private void AssignPlanetToButton(GameObject planet, GameObject button)
+        {
+            var planetButton = button.AddComponent<PlanetButton>();
+            planetButton.Planet = planet;
+            button.GetComponentInChildren<Text>().text = planet.name;
+            button.GetComponent<Button>().onClick.AddListener(planetButton.ChangeFocusToThis);
+        }
+    
+        private void CheckSerializedFields()
+        {
+            if (ui is null) 
+                Debug.LogError(": Panel not assigned",this);
+    
+            if (buttonPrefab is null)
+                Debug.LogError(": Button Prefab not assigned.", this);
+        }
     }
 }
